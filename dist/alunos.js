f4e46ee93172e1b53f7c0059fe3b994694d76d16
Cieliocas <francielioevangelista@hotmail.com>
2024-12-17 19:53:05 -0300

    Aprovados e reprovados

--- dist/alunos.js
+++ dist/alunos.js
@@ -24,3 +24,32 @@ var notasDosAlunos = [
   nome: 'Priscila',
   nota: 6
 }];
+
+// Filtrar alunos com nota maior ou igual a 7
+var alunosAprovados = notasDosAlunos.filter(function (aluno) {
+  return aluno.nota >= 6;
+});
+
+// Alunos que reprovaram
+var alunosReprovados = notasDosAlunos.filter(function (aluno) {
+  return aluno.nota < 6;
+});
+
+// Exibir alunos que foram aprovados em uma variavel com frase formatada
+var aprovados = alunosAprovados.map(function (aluno) {
+  return "".concat(aluno.nome, " foi aprovado com nota ").concat(aluno.nota);
+});
+
+// Exibir alunos que foram reprovados em uma variavel com frase formatada
+var reprovados = alunosReprovados.map(function (aluno) {
+  return "".concat(aluno.nome, " foi reprovado com nota ").concat(aluno.nota);
+});
+
+// printar alunos aprovados
+console.log(aprovados);
+
+// printar alunos reprovados
+console.log(reprovados);
+
+// Exemplo de saida
+// [ 'Gian foi aprovado com nota 9', 'Cielio foi aprovado com nota 7', 'Helena foi aprovado com nota 10', 'Priscila foi aprovado com nota 6' ]
--- src/alunos.js
+++ src/alunos.js
@@ -8,3 +8,23 @@ const notasDosAlunos = [ // Array de objetos
     { nome: 'Priscila', nota: 6 },
 ];
 
+// Filtrar alunos com nota maior ou igual a 7
+const alunosAprovados = notasDosAlunos.filter(aluno => aluno.nota >= 6);
+
+// Alunos que reprovaram
+const alunosReprovados = notasDosAlunos.filter(aluno => aluno.nota < 6);
+
+// Exibir alunos que foram aprovados em uma variavel com frase formatada
+const aprovados = alunosAprovados.map(aluno => `${aluno.nome} foi aprovado com nota ${aluno.nota}`);
+
+// Exibir alunos que foram reprovados em uma variavel com frase formatada
+const reprovados = alunosReprovados.map(aluno => `${aluno.nome} foi reprovado com nota ${aluno.nota}`);
+
+// printar alunos aprovados
+console.log(aprovados);
+
+// printar alunos reprovados
+console.log(reprovados);
+
+// Exemplo de saida
+// [ 'Gian foi aprovado com nota 9', 'Cielio foi aprovado com nota 7', 'Helena foi aprovado com nota 10', 'Priscila foi aprovado com nota 6' ]
