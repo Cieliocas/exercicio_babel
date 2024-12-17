d5e1c61f89c434f3bf68cc99f4c5b41748242a4f
Cieliocas <francielioevangelista@hotmail.com>
2024-12-17 19:52:49 -0300

    alunos

--- dist/alunos.js
+++ dist/alunos.js
@@ -1,1 +1,26 @@
 "use strict";
+
+var notasDosAlunos = [
+// Array de objetos
+{
+  nome: 'Gian',
+  nota: 9
+}, {
+  nome: 'Cielio',
+  nota: 7
+}, {
+  nome: 'Lucas',
+  nota: 5
+}, {
+  nome: 'Pedro',
+  nota: 4
+}, {
+  nome: 'Joana',
+  nota: 3
+}, {
+  nome: 'Helena',
+  nota: 10
+}, {
+  nome: 'Priscila',
+  nota: 6
+}];
---
+++ src/alunos.js
@@ -0,0 +1,10 @@
+const notasDosAlunos = [ // Array de objetos
+    { nome: 'Gian', nota: 9 },
+    { nome: 'Cielio', nota: 7 },
+    { nome: 'Lucas', nota: 5 },
+    { nome: 'Pedro', nota: 4 },
+    { nome: 'Joana', nota: 3 },
+    { nome: 'Helena', nota: 10 },
+    { nome: 'Priscila', nota: 6 },
+];
+
